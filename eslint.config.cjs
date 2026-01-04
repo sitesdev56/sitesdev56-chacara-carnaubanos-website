d4601cab18939c591d00c47420c6561d5c5ae732
Diego Lima <diegoxiaomi56@gmail.com>
2026-01-04 00:22:29 +0000

    chore: convert to flat ESLint config format (eslint.config.cjs)

--- eslint.config.cjs
+++ eslint.config.cjs
@@ -1,26 +1,26 @@
-module.exports = {
-  ignores: ['.next', 'node_modules', 'public', 'dist', 'build', '.env'],
-  languageOptions: {
-    ecmaVersion: 2020,
-    sourceType: 'module',
-    ecmaFeatures: { jsx: true }
+module.exports = [
+  {
+    ignores: ['.next', 'node_modules', 'public', 'dist', 'build', '.env']
   },
-  plugins: {
-    '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
-    react: require('eslint-plugin-react')
-  },
-  settings: {
-    react: { version: 'detect' }
-  },
-  overrides: [
-    {
-      files: ['**/*.{js,jsx,ts,tsx}'],
-      parser: require('@typescript-eslint/parser'),
-      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'next/core-web-vitals'],
-      rules: {
-        'react/react-in-jsx-scope': 'off',
-        '@typescript-eslint/explicit-module-boundary-types': 'off'
-      }
+  {
+    files: ['**/*.{js,jsx,ts,tsx}'],
+    languageOptions: {
+      ecmaVersion: 2020,
+      sourceType: 'module',
+      ecmaFeatures: { jsx: true }
+    },
+    plugins: {
+      '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+      react: require('eslint-plugin-react')
+    },
+    settings: {
+      react: { version: 'detect' }
+    },
+    parser: require('@typescript-eslint/parser'),
+    extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'next/core-web-vitals'],
+    rules: {
+      'react/react-in-jsx-scope': 'off',
+      '@typescript-eslint/explicit-module-boundary-types': 'off'
     }
-  ]
-};
+  }
+];
