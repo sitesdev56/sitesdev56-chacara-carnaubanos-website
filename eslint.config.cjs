5911722c5c6441acddcb70d896251f9ed7708073
Diego Lima <diegoxiaomi56@gmail.com>
2026-01-04 00:20:58 +0000

    chore: migrate to eslint.config.cjs and add ignores

---
+++ eslint.config.cjs
@@ -0,0 +1,27 @@
+module.exports = {
+  root: true,
+  ignores: ['.next', 'node_modules', 'public', 'dist', 'build', '.env'],
+  languageOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: { jsx: true }
+  },
+  plugins: {
+    '@typescript-eslint': require('@typescript-eslint/eslint-plugin'),
+    react: require('eslint-plugin-react')
+  },
+  settings: {
+    react: { version: 'detect' }
+  },
+  overrides: [
+    {
+      files: ['**/*.{js,jsx,ts,tsx}'],
+      parser: require('@typescript-eslint/parser'),
+      extends: ['eslint:recommended', 'plugin:@typescript-eslint/recommended', 'plugin:react/recommended', 'next/core-web-vitals'],
+      rules: {
+        'react/react-in-jsx-scope': 'off',
+        '@typescript-eslint/explicit-module-boundary-types': 'off'
+      }
+    }
+  ]
+};
